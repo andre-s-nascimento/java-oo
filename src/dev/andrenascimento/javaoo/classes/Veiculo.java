499242d252871aaebec4d3ecca567d5a00395835
André Nascimento <Andre.Nascimento.Contractor@pepsico.com>
2024-10-11 14:15:27 -0300

    first commit

---
+++ src/dev/andrenascimento/javaoo/classes/Veiculo.java
@@ -0,0 +1,21 @@
+package dev.andrenascimento.javaoo.classes;
+
+public class Veiculo {
+    
+    public String nome;
+
+    public String marca;
+
+    public String chassi;
+
+    public int quantidadeRodas;
+
+    public void ligar() {
+        System.out.println("O veículo ligou!");
+    }
+
+    public void deligar() {
+        System.out.println("O veículo desligou!");
+    }
+
+}
